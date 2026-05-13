bf83de920883abf0e169fde82d3345e0ca55aac2
Mel <melissasilva245@gmail.com>
2026-05-13 19:23:30 -0300

    foi configurado a parte da criptografia da senha do usuário e o JWT esta em andamento

--- gerenciamento/src/main/java/br/com/mellix/gerenciamento/service/ProfileService.java
+++ gerenciamento/src/main/java/br/com/mellix/gerenciamento/service/ProfileService.java
@@ -1,11 +1,20 @@
 package br.com.mellix.gerenciamento.service;
 
+import br.com.mellix.gerenciamento.dto.AuthDTO;
 import br.com.mellix.gerenciamento.dto.ProfileDTO;
 import br.com.mellix.gerenciamento.entity.ProfileEntity;
 import br.com.mellix.gerenciamento.repository.ProfileRepository;
+import br.com.mellix.gerenciamento.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -14,6 +23,9 @@ public class ProfileService {
 
     private final ProfileRepository profileRepository;
     private final EmailService emailService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     public ProfileDTO registerProfile (ProfileDTO profileDTO) {
         ProfileEntity newProfile = toEntity(profileDTO);
@@ -32,7 +44,7 @@ public class ProfileService {
                 .id(profileDTO.getId())
                 .fullName(profileDTO.getFullName())
                 .email(profileDTO.getEmail())
-                .password(profileDTO.getPassword())
+                .password(passwordEncoder.encode(profileDTO.getPassword()))
                 .profileImageUrl(profileDTO.getProfileImageUrl())
                 .createdAt(profileDTO.getCreatedAt())
                 .updatedAt(profileDTO.getUpdatedAt())
@@ -58,5 +70,49 @@ public class ProfileService {
                     return true;
                 })
                 .orElse(false);
+    }
+
+    public boolean isAccountActive(String email) {
+        return profileRepository.findByEmail(email)
+                .map(ProfileEntity::getIsActive)
+                .orElse(false);
+    }
+
+    public ProfileEntity getCurrentProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return profileRepository.findByEmail(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("Perfil com este email não foi encontrado: "+ authentication.getName()));
+    }
+
+    public ProfileDTO getPublicProfile(String email) {
+        ProfileEntity currentUser = null;
+        if (email == null) {
+            currentUser = getCurrentProfile();
+        } else {
+            currentUser = profileRepository.findByEmail(email)
+                    .orElseThrow(() -> new UsernameNotFoundException("Perfil com este email não foi encontrado "+ email));
+        }
+
+        return ProfileDTO.builder()
+                .id(currentUser.getId())
+                .fullName((currentUser.getFullName()))
+                .email(currentUser.getEmail())
+                .profileImageUrl(currentUser.getProfileImageUrl())
+                .createdAt(currentUser.getCreatedAt())
+                .updatedAt(currentUser.getUpdatedAt())
+                .build();
+    }
+
+    public Map<String, Object> authenticateAndGenerateToken(AuthDTO authDTO) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authDTO.getEmail(), authDTO.getPassword()));
+            String token = jwtUtil.generateToken(authDTO.getEmail());
+            return Map.of(
+                    "token", token,
+                    "user", getPublicProfile(authDTO.getEmail())
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
